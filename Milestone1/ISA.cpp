1919f651844a18c34c5d4608aca7a0de5cace296
Johannes Lee <jlee310@uncc.edu>
2020-02-27 12:38:28 +0000

    code written for parsing the actual asm instruction

--- Milestone1/ISA.cpp
+++ Milestone1/ISA.cpp
@@ -22,7 +22,7 @@ int main(){
 
 //erase comments
 for (int i = 0; i < inLine.size(); i++){
-    if (inLine[i][0] == '-' || inLine[i][0] == '0'){
+    if (inLine[i][0] == '-' || isdigit(inLine[i][0])){
         inLine.erase(inLine.begin() + i);
         i--;
     }
@@ -43,21 +43,72 @@ for (int i = 0; i < inLine.size(); i++){
 //erase commas and spaces
 for (int i = 0; i < inLine.size(); i++){
 replace( inLine[i].begin(), inLine[i].end(), ',', '\0');  
-replace( inLine[i].begin(), inLine[i].end(), ' ', '\0');  
+//replace( inLine[i].begin(), inLine[i].end(), ' ', '\0');  
 }
-  
+
 
   
-  
-  
-    for (auto i: inLine){
+//remove end spaces
+for (int i = 0; i < inLine.size(); i++){
+    while(inLine[i].size() && isspace(inLine[i].back())){
+        inLine[i].pop_back();
+    }
+}
+
+
+  string a = "";
+  int counter1 = 0;
+
+
+vector<string>temp;
+
+//parsing
+for (int i = 0; i < inLine.size(); i++){
+   for (int j = 0; j < inLine[i].length(); j++){
+       
+     
+ 
+      
+     
+       try{
+        a = inLine[i].substr(inLine[i].rfind(' ')+1,inLine[i].length());
+        inLine[i].replace(inLine[i].rfind(' '),inLine[i].length(),"");
+        temp.push_back(a);
+        
+     //    cout << a << endl;
+       }catch(exception& e){
+          
+       }
+        
+       
+    }
+     if(counter1 == 0){
+                a = inLine[i].substr(0, inLine[i].find(" "));
+                temp.push_back(a);
+                reverse(temp.begin(),temp.end());  
+                instruct.insert(instruct.end(), temp.begin(), temp.end());
+                temp.clear();
+              //  cout << a << endl;
+                counter1++;
+               
+            }
+    
+    
+    counter1 = 0;
+}
+
+
+
+
+
+    for (auto i: instruct){
         
         
         
       cout << i <<endl;
     }
     
-    
+   
     
     return 0;
 }
